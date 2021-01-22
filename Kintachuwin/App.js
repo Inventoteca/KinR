22d662dc0bb7600c5ddb1abfebe7cd18c231b6dc
Glioce <17276408+Glioce@users.noreply.github.com>
2021-01-22 01:53:22 -0600

    librerias instaladas

---
+++ Kintachuwin/App.js
@@ -0,0 +1,151 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Sound from 'react-native-sound'; //para reproducir sonidos
+import YoutubePlayer from "react-native-youtube-iframe";
+
+function playLocalSound() {
+  //Sound.stop();
+  Sound.setCategory('Playback', false);
+  var mySound = new Sound('au_1_01.mp3', Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+      console.log('Error al cargar: ' + error);
+      return;
+    }
+    else {
+      mySound.play((success) => {
+        if (success) {
+          console.log('Sonado');
+        }
+        else {
+          console.log('Problema al tocar');
+        }
+      })
+    }
+  });
+  mySound.setVolume(0.9);
+  mySound.release();
+}
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+              
+              <TouchableOpacity style={{ backgroundColor: 'lightgrey', justifyContent: 'center' }} onPress={() => playLocalSound()}>
+                <Text style={{ textAlign: 'center', fontSize: 18, color: 'black', padding: 5, fontFamily: "Purisa" }}>Play sound from local file</Text>
+              </TouchableOpacity>
+
+              <YoutubePlayer height={250} videoId={"sNhhvQGsMEc"} />
+
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    flex:1,
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    //fontWeight: '400',
+    fontFamily: 'Purisa',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
